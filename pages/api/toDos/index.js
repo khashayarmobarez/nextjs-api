95e2c62e1e4eeb496f16132dd030d41a3ae479a5
khashayar <khashayarmobarezbusiness@gmail.com>
2024-11-21 15:12:38 +0330

    Implement CRUD operations for to-do items in API and update UI for adding and deleting tasks

--- pages/api/toDos/index.js
+++ pages/api/toDos/index.js
@@ -1,5 +1,25 @@
 import { todos } from "@/data/todos"
 
 export default function handler (req,res) {
-    res.status(200).json(todos);
+    if(req.method === 'GET') {
+        res.status(200).json(todos);
+    } else if(req.method === 'POST') {
+        const todo = req.body.todo;
+
+        // store data
+        const newToDo = {
+            id: todos.length + 1,
+            title: todo
+        }
+
+        res.status(201).json({
+            message:'success',
+            data: newToDo
+        })
+    } else if( req.method === 'DELETE') {
+        res.status(200).json({
+            message: 'delete successfuly',
+            data: []
+        })
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 export default function Home() {
 
   const [toDos, setToDos ] = useState([])
+  const [addToDo, setAddToDo] = useState('')
 
   useEffect(() => {
     async function fetchData() {
@@ -18,6 +19,28 @@ export default function Home() {
 
   },[])
 
+  
+  const clickHandler = async () => {
+    event.preventDefault();
+
+    const res = await fetch('api/toDos', {
+      method: 'POST',
+      body: JSON.stringify({todo: addToDo}),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+    const data = await res.json();
+    console.log(data)
+  };
+
+  const deleteHandler = async () => {
+    const res = await fetch('/api/toDos', {
+      method: 'DELETE',
+    });
+    const data = await res.json();
+    console.log(data);
+  };
 
 
   return (
@@ -27,6 +50,13 @@ export default function Home() {
           <li key={task.id}>{task.title}</li>
         )}
       </ul>
+      <div>
+        <input value={addToDo} onChange={(e) => setAddToDo(e.target.value)} />
+        <button onClick={clickHandler} >create to do</button>
+      </div>
+      <div >
+        <button onClick={deleteHandler}>delete all</button>
+      </div>
     </div>
   );
 }
